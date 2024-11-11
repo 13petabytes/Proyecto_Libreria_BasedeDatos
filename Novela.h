501c86e10d43bc344848ef15e911664cef1f4387
13petabytes <150091598+13petabytes@users.noreply.github.com>
2024-11-11 11:23:24 -0600

    Update Novela.h

--- Novela.h
+++ Novela.h
@@ -7,8 +7,13 @@
 #include "Item.h"
 using namespace std;
 
+//Definición de la subclase Novela
+//Al igual que en la clase libro, se hace la aclaración que la diferenciación entre libros academicos, aqui titulados como libros, y novelas
+//se efectua con el fin de imitar la dispoción u ordenamientos de libros que se hacen en las bibliotecas
+
 class Novela : public Item {
 private:
+//Variable int que ayuda a la modificación de la variable numero, heredada de Item
   int novelaN;
 public:
   //constructor y destructor
@@ -18,7 +23,10 @@ public:
   int getNovelaN();
   void setNovelaN(int);
   //metodos
+  //Se detallan abajo
+  //Se encarga de definir la variable heredada de item, numero
   void unirTXT() override;
+  //Se encarga de solicitarle los datos al usuario apra el id
   void crearItem() override;
 };
 
@@ -37,21 +45,44 @@ int Novela::getNovelaN() {
 }
 
 //metodos
+//En esta clase, las sentenas que diferencian al id como uno de una novela, es el numero 300
 void Novela::unirTXT() {
-  numero = '3' + novelaN;
+  numero = 300 + novelaN;
 }
 
 void Novela::crearItem() {
+  //se definen variables
   int pasta;
+  int genero;
+  //Se le soliciatan datos al usuario
   cout<<"Incerte el tipo de pasta de la novela:"<<endl;
   cout<<"1) para Dura"<<endl;
   cout<<"2) para Blanda"<<endl;
   cin>>pasta;
+  //W*
   while (pasta != 1 && pasta != 2){
     cout<<"Incerte una opción valida"<<endl;
     cout<<"1) para Dura"<<endl;
     cout<<"2) para Blanda"<<endl;
     cin>>pasta;
   };
-  novelaN = pasta;
+  cout << "Seleccione el género de la novela:" << endl;
+  cout << "1) Ciencia Ficción" << endl;
+  cout << "2) Fantasía" << endl;
+  cout << "3) Misterio" << endl;
+  cout << "4) Romance" << endl;
+  cout << "5) Terror" << endl;
+  cin >> genero;
+  //W*
+  while (genero < 1 || genero > 5) {
+    cout << "Inserte una opción válida" << endl;
+    cout << "1) Ciencia Ficción" << endl;
+    cout << "2) Fantasía" << endl;
+    cout << "3) Misterio" << endl;
+    cout << "4) Romance" << endl;
+    cout << "5) Terror" << endl;
+    cin >> genero;
+  }
+  //Se unifica todo en la variable novelaN
+  novelaN = genero*10 +pasta;
 }
