5dd62fbac760d6d15328f19fef59e0e1ad84dfde
13petabytes <150091598+13petabytes@users.noreply.github.com>
2024-11-11 11:24:03 -0600

    Update Registrador.h

--- Registrador.h
+++ Registrador.h
@@ -10,29 +10,38 @@
 
 using namespace std;
 
+//Definición de la clase que se encarga de manipular el arbol
+//para los casos de uso de las funciones, al ser definidos los mejores, peores y promedios de las mismas, se empleara n para simbolizar el numero de nodos
+//se aclara que el arbol no se ordena, por lo que al agregar más valores, tendera a desvalancearse
+
 template <class T> class Registrador;
 
 template <class T>
 class Node {
 private:
+	//Variables
 	T value;
 	Node *left, *right;
 
+	//función privada que aun no se encuentra en un estado ene l que el usuario al use
 	Node<T>* succesor();
 
 public:
+	//Constructores
 	Node(T);
 	Node(T, Node<T>*, Node<T>*);
+	//Metodos actualmente usados
 	void add(T);
 	bool find(T);
+	void inorder(std::stringstream&) const;
+	//Metodos para funciones para opciones proccimas a ser agregadas
 	void remove(T);
 	void removeChilds();
-	void inorder(std::stringstream&) const;
-	void preorder(std::stringstream&) const;
-
+	//Se aclara que la clase registrador es amiga
 	friend class Registrador<T>;
 };
 
+//Se definen contrctores
 template <class T>
 Node<T>::Node(T val) : value(val), left(0), right(0) {}
 
@@ -41,7 +50,13 @@ Node<T>::Node(T val, Node<T> *le, Node<T> *ri)
 	: value(val), left(le), right(ri) {}
 
 template <class T>
+
 void Node<T>::add(T val) {
+	//Se define la función add
+	//para los casos de esta función se hace un simil con la clase busqueda
+	//El caso promedio para esta función es que el valor a agregar sea O(log n), esto en un desbalanceado
+	//En el mejor caso seria igual de O(log n), pero estando el arbol balanceado
+	//En el peor caso se agregaria el valor al final de un arbol desvalanciado, siaendo similar agregarlo a la cola de una lista, O(n)
 	if (val < value) {
 		if (left != 0) {
 			left->add(val);
@@ -59,6 +74,10 @@ void Node<T>::add(T val) {
 
 template <class T>
 bool Node<T>::find(T val) {
+	//Se define la función find
+	//para esta función nuestro mejor caso de busqueda, ignorando al nodo raíz, se nos presenta si el arbol esta balanciado, lo caul si el arbol es de pocos valores va a tender a serlo, O(log n)
+	//nuestro caso promedio, siendo si el arbol esta desbalanceado, nuestra notación big O se designa como O(logn)
+	//el peor caso se da si el valor se encuentra al final del arbol, estando este desbalanceado, en esta se recorrerian todos los nodos, O(n)
 	if (val == value) {
 		return true;
 	} else if (val < value) {
@@ -70,6 +89,12 @@ bool Node<T>::find(T val) {
 
 template <class T>
 Node<T>* Node<T>::succesor() {
+	//se define la función succesor
+	//en esta los esenarios no varian de la función find, porloque solo se aclaran su notación asintótica
+	//mejor caso: O(log n)
+	//caso promedio: O(log n)
+	//peor caso: O(n)
+
 	Node<T> *le, *ri;
 
 	le = left;
@@ -102,6 +127,11 @@ Node<T>* Node<T>::succesor() {
 
 template <class T>
 void Node<T>::remove(T val) {
+	//se define la función remove
+	//en esta los esenarios no varian de la función find, porloque solo se aclaran su notación asintótica
+	//mejor caso: O(log n)
+	//caso promedio: O(log n)
+	//peor caso: O(n)
 	Node<T> * succ, *old;
 
 	if (val < value) {
@@ -139,6 +169,10 @@ void Node<T>::remove(T val) {
 
 template <class T>
 void Node<T>::removeChilds() {
+	//en este caso, y diferenciandonos de los casos anteriores, nuestros casos promedios y mejores nov avarian, pero si lo haria el peor, disminullendose la cantidad de nodos a recorrer en 1
+	//mejor caso: O(log n)
+	//caso promedio: O(log n)
+	//peor caso: O(n - 1)
 	if (left != 0) {
 		left->removeChilds();
 		delete left;
@@ -153,6 +187,7 @@ void Node<T>::removeChilds() {
 
 template <class T>
 void Node<T>::inorder(std::stringstream &aux) const {
+	//se define la función inorder, que se emplea en la clase Registrador para almacenar en el txt
 	if (left != 0) {
 		left->inorder(aux);
 	}
@@ -165,24 +200,15 @@ void Node<T>::inorder(std::stringstream &aux) const {
 	}
 }
 
-template <class T>
-void Node<T>::preorder(std::stringstream &aux) const {
-	aux << value;
-	if (left != 0) {
-		aux << " ";
-		left->preorder(aux);
-	}
-	if (right != 0) {
-		aux << " ";
-		right->preorder(aux);
-	}
-}
 
 using namespace std;
+
+//Definición de clase encargada de la interacción/comunicación con el main y el usuario, con las funciones de la clase Node
 
 template <class T>
 class Registrador {
 private:
+	//definición de variables
     Node<T> *root;
 public:
     // Constructor y destructor
@@ -191,65 +217,94 @@ public:
 
     // Métodos
 
-//arbol binario
+	
+	bool empty() const{
+		//Función para verificar que el arbol no este vasio
+		return (root == 0);
+	};
 
-      bool empty() const{
-    return (root == 0);
-  };
+	void add(T val){
+	//Llamado de la función ad tras la confirmación de que no existe el numero
+	//Los casos para esta función serian el resultado de la convicación de los de add y el peor de find, esto puesto a que se recorreria todo el arbol si no existe el valor, quedando así
+	//mejor: O(log n) + O(n)
+	//peor: O(2n)
+	//promedio: O(log n) + O(n)
+		if(root != 0){
+		if (!root->find(val)){
+			root->add(val);
+		}
+		} else {
+		root = new Node<T>(val);
+	}};
 
-    void add(T val){
-    if(root != 0){
-      if (!root->find(val)){
-        root->add(val);
-      }
-    } else {
-      root = new Node<T>(val);
-    }};
+	void archivar() {
+		//Se archiva el contenido del arbol, ordenado por el metodo "inorder"
+		if (root == nullptr) {
+			cout << "El árbol está vacío. No hay nada que archivar." << endl;
+			return;
+		}
 
-    void archivar(){
-      stringstream aux;
-      if (!empty()){
+		//Se almacena el contenido del arbol en una variable strinbgstram, para posteriormente guardarlo en el archivo
 
-        root->inorder(aux);
+		ofstream archivo("Archibaje.txt",  ios::app);  // Abrir el archivo en modo de escritura
+		if (!archivo.is_open()) {
+			cerr << "Error al abprir el archivo." << endl;
+			return;
+		}
 
-        ofstream archi("Archibaje.txt");
+		stringstream aux;
+		root->inorder(aux);  // Llama a inorder para llenar aux con el contenido del árbol
 
-        if (archi.is_open()){
-          archi << aux.str();
-          archi.close();
-          cout << endl << "El contenido se ha guardado en archivo.txt" << endl;
-        }
-      } else{
-        cout<<endl<<"Error, no hay nada para archivar"<< endl;
-      }
-  };
+		if (aux.str().empty()) {
+			cout << "Advertencia: El stringstream está vacío después de llamar a inorder." << endl;
+		} else {
+			archivo << aux.str();
+			cout << "El contenido se ha guardado en Archibaje.txt" << endl;
+		}
+
+		archivo.close();
+	}
+
 
     void buscar(T val) const{
-    if (root != 0) {
-      bool x = root->find(val);
-      if (x == true){
-        cout << endl << "Se encuentra en la base de datos." << endl;
-      } else{
-        cout << endl << "No se encuentra en la base de datos."<< endl;
-      }
-    } else {
-      cout << endl << "Error" << endl;
-    }
-  };
+		//Llamado de la función find de la clase Node
+		//Los casos son los mismos que con la clase find
+		//mejor caso: O(log n)
+		//caso promedio: O(log n)
+		//peor caso: O(n)
+
+		if (root != 0) {
+		bool x = root->find(val);
+		if (x == true){
+			cout << endl << "Se encuentra en la base de datos." << endl;
+		} else{
+			cout << endl << "No se encuentra en la base de datos."<< endl;
+		}
+		} else {
+		cout << endl << "Error" << endl;
+		}};
 
   void comerTXT(){
+	//Esta función se encarga de almacenar el contenido del archivo de texto en el arbol binario, para posterior mente borrar el contenidod el mismo
+	//Con esto se requiere que la unción archivar sea la ultima en ser ejecutada en el programa
+	//Tambien se garantiza el correcto almasenamiento de la función archivar 
+	//Se abre archivo
     ifstream archi("Archibajetxt");
     if(!archi) {
       cerr << "Error al abrir el archivo."<<endl;
     }
+	//se definen variables
     string auxstr;
     stringstream aux;
     aux << archi.rdbuf();
     if (!aux.str().empty()){
+	//se incertan los datos del txt en el arbol, tras transformar la variable auxstr en int con stoi()
       while(getline(archi, auxstr)){
         add(stoi(auxstr));
       }
     }
+	//se sierra el archivo, para posteriormente borrarlo
+	//se hace en este orden porque resulto menos problematico al compilar
     archi.close();
     std::ofstream archi2("Archibaje.txt", std::ios::trunc);
   };
